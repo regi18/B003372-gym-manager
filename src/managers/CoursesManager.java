efa47f182f427a1b949f49c0f1974695142c0ea7
Simone Carletti <regi18@outlook.it>
2023-03-14 12:04:23 +0100

    test: CoursesManager

--- src/managers/CoursesManager.java
+++ src/managers/CoursesManager.java
@@ -8,6 +8,11 @@ import java.util.ArrayList;
 public class CoursesManager {
     private final ArrayList<Course> courses = new ArrayList<>();
 
+    /**
+     * Adds a new course to the list
+     * @param c The course to add
+     * @throws IllegalArgumentException In case the given course is already present in the list
+     */
     public void addCourse(Course c) throws IllegalArgumentException {
         if (getCourse(c.getId()) != null)
             throw new IllegalArgumentException("A course with this id exists already");
@@ -15,6 +20,11 @@ public class CoursesManager {
         this.courses.add(c);
     }
 
+    /**
+     * Deletes the given course from the list
+     * @param id The id of the course to delete
+     * @return true if successful, false otherwise
+     */
     public boolean deleteCourse(int id) {
         Course toRemove = getCourse(id);
         if (toRemove == null) return false;
@@ -27,6 +37,11 @@ public class CoursesManager {
         return res;
     }
 
+    /**
+     * Returns the given course
+     * @param id The course id to fetch
+     * @return The course
+     */
     public Course getCourse(int id) {
         for (Course c : courses) {
             if (c.getId() == id) return c;
---
+++ test/managers/CoursesManagerTest.java
@@ -0,0 +1,53 @@
+package managers;
+
+import models.Course;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.time.LocalDateTime;
+import org.mockito.Mockito;
+
+
+class CoursesManagerTest {
+    private CoursesManager m;
+    private Course testCourse;
+
+    @Test
+    @BeforeEach
+    public void when_AddingNewCourse_Expect_Success() {
+        m = new CoursesManager();
+        testCourse = new Course("test", 10, LocalDateTime.now(), LocalDateTime.now().plusHours(1));
+        m.addCourse(testCourse);
+    }
+
+    @Test
+    public void when_AddingAlreadyExistingCourse_Expect_IllegalArgumentException() {
+        Assertions.assertThrows(
+            IllegalArgumentException.class,
+            () -> m.addCourse(testCourse),
+            "Expected addCourse() to throw, but it didn't"
+        );
+    }
+
+    @Test
+    public void when_gettingExistingCourse_Expect_toReturnThatCourse() {
+        Course c1 = new Course("test2", 10, LocalDateTime.now(), LocalDateTime.now().plusHours(1));
+        m.addCourse(c1);
+        Assertions.assertEquals(testCourse, m.getCourse(testCourse.getId()));
+    }
+
+    @Test
+    public void when_gettingNonExistingCourse_Expect_toReturnNull() {
+        Assertions.assertNull(m.getCourse(testCourse.getId() + 1));
+    }
+
+    @Test
+    public void when_deletingExistingCourse_Expect_toReturnTrueAndNotifySubscribers() {
+        Assertions.assertTrue(m.deleteCourse(testCourse.getId()));
+    }
+
+    @Test
+    public void when_deletingNonExistingCourse_Expect_toReturnFalse() {
+        Assertions.assertFalse(m.deleteCourse(testCourse.getId() + 1));
+    }
+}
