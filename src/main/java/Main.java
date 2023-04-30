c0dde71c8cea0906ce2752e137db72e720db8178
Andrea Delli <andrea.delli2001@gmail.com>
2023-04-30 18:07:35 +0200

    update main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,5 @@
-import controllers.BookingsController;
-import controllers.CoursesController;
-import controllers.PeopleController;
-import models.Course;
-import models.Customer;
-import models.Trainer;
-import models.membership.Membership;
-import models.membership.WeekendMembershipDecorator;
+import controllers.*;
+import dao.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -13,27 +7,28 @@ import java.time.LocalDateTime;
 
 public class Main {
     public static void main(String[] args) {
-//        Trainer trainer1 = new Trainer("A", "B", "C", 10);
-//
-//        // create sample memberships
-//        Membership yearlyFullMembership = new FullMembership(300, LocalDate.now(), LocalDate.now().plusYears(1));
-//        Membership yearlyWeekendMembership = new WeekendMembershipDecorator(yearlyFullMembership);
-//
-//        CoursesController coursesController = new CoursesController();
-//        BookingsController bookingsController = new BookingsController(coursesController);
-//        PeopleController<Customer> customersController = new PeopleController<>();
-//        PeopleController<Trainer> trainersController = new PeopleController<>();
-//
-//        // Add sample trainers
-//        trainersController.addPerson(trainer1);
-//
-//        // Add sample customers
-//        customersController.addPerson(new Customer("RSSMRA000", "Mario", "Rossi", yearlyFullMembership));
-//        customersController.addPerson(new Customer("VRDLGI111", "Luigi", "Verdi", yearlyWeekendMembership));
-//
-//        // Add sample courses
-//        coursesController.addCourse(new Course("Spinning", 10, LocalDateTime.now().plusHours(1), LocalDateTime.now().plusHours(3), trainer1));
-//        coursesController.addCourse(new Course("Calisthenics", 10, LocalDateTime.now().plusHours(1), LocalDateTime.now().plusHours(3), trainer1));
-//        coursesController.addCourse(new Course("Yoga", 10, LocalDateTime.now().plusHours(1), LocalDateTime.now().plusHours(3), trainer1));
+
+        // DAOs
+        TrainerDAO trainerDAO = new TrainerDAOsqlite();
+        MembershipDAO membershipDAO = new MembershipDAOsqlite();
+        CustomerDAO customerDAO = new CustomerDAOsqlite(membershipDAO);
+        CourseDAO courseDAO = new CourseDAOsqlite(trainerDAO, customerDAO);
+
+        // Controllers
+        TrainersController trainersController = new TrainersController(trainerDAO);
+        CustomersController customersController = new CustomersController(customerDAO);
+        CoursesController coursesController = new CoursesController(trainersController, courseDAO);
+
+        // Add sample trainers
+        trainersController.addPerson("A", "B", "C", 10);
+
+        // Add sample customers
+        customersController.addPerson("RSSMRA000", "Mario", "Rossi", new String[]{"weekend", "weekdays"}, LocalDate.now().plusYears(1));
+        customersController.addPerson("VRDLGI111", "Luigi", "Verdi", new String[]{"weekend"}, LocalDate.now().plusYears(1));
+
+        // Add sample courses
+        coursesController.addCourse("Spinning", 10, LocalDateTime.now().plusHours(1), LocalDateTime.now().plusHours(3), "A");
+        coursesController.addCourse("Calisthenics", 10, LocalDateTime.now().plusHours(1), LocalDateTime.now().plusHours(3), "A");
+        coursesController.addCourse("Yoga", 10, LocalDateTime.now().plusHours(1), LocalDateTime.now().plusHours(3), "A");
     }
 }
